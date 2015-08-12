5c0891b6099ceb19406a914f35a2a6cc7caabad9
Santiago Aceñolaza <chanacenolaza@gmail.com>
2015-08-12 13:07:13 -0300

    Updated Core and ServerConnector nuget packages

--- VersionOne.Integration.Tfs.Listener.Tests/Properties/AssemblyInfo.cs
+++ VersionOne.Integration.Tfs.Listener.Tests/Properties/AssemblyInfo.cs
@@ -4,14 +4,14 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("4.0.1.0")]
-[assembly: AssemblyFileVersion("4.0.1.0")]
+[assembly: AssemblyVersion("4.0.3.0")]
+[assembly: AssemblyFileVersion("4.0.3.0")]
 [assembly: AssemblyInformationalVersion("See https://github.com/versionone/V1TFS/wiki")]
 
 [assembly: AssemblyProduct("VersionOne.Integration.Tfs.Listener.Tests")]
 [assembly: AssemblyTitle("VersionOne.Integration.Tfs.Listener.Tests")]
 [assembly: AssemblyDescription("VersionOne Team Foundation Server Integration")]
 [assembly: AssemblyCompany("VersionOne, Inc.")]
-[assembly: AssemblyCopyright("Copyright 2008-2014, VersionOne, Inc., Licensed under modified BSD.")]
+[assembly: AssemblyCopyright("Copyright 2008-2015, VersionOne, Inc., Licensed under modified BSD.")]
 
 [assembly: AssemblyConfiguration("Debug")]
